914dd624e9738f56df2947c59c9169e21f58d183
陈玲琳 <chenlinglin@chenlinglindeMacBook-Pro.local>
2023-03-19 14:53:27 -0400

    completed

--- functions.py
+++ functions.py
@@ -70,38 +70,72 @@ PART TWO:
 
 # 1. Write a function called 'hello_world' that does not take any arguments and
 #    prints "Hello World".
-
+def hello_world():
+    print("Hello World")
+hello_world()
 
 # 2. Write a function called 'say_hi' that takes a name as a string and
 #    prints "Hi" followed by the name.
+def say_hi(name):
+    print(f"Hi {name}")
+say_hi("Lynne")
 
 
 # 3. Write a function called 'print_product' that takes two integers and
 #    multiplies them together. Print the result.
-
+def print_product(num1, num2):
+    print(num1 * num2)
+print_product(5,4)
 
 # 4. Write a function called 'repeat_string' that takes a string and an integer
 #    and prints the string that many times
-
+def repeat_string(string, num2):
+    print(string * num2)
+repeat_string("Hello, China! ",5)
 
 # 5. Write a function called 'print_sign' that takes an integer and prints
 #    "Higher than 0" if higher than zero and "Lower than 0" if lower than zero.
 #    If the integer is zero, print "Zero".
+def print_sign(num):
+    if num > 0:
+        print("Higher than 0")
+    elif 0 > num:
+        print("Lower than 0")
+    else:
+        print("Zero")
+print_sign(-4)
+
 
 
 # 6. Write a function called 'is_divisible_by_three' that takes an integer and
 #    returns a boolean (True or False), depending on whether the number is
 #    evenly divisible by 3.
+def is_divisible_by_three(num):
+    return num % 3 == 0
+print(is_divisible_by_three(15))
+print(is_divisible_by_three(10))
+print(is_divisible_by_three(18))
 
 
 # 7. Write a function called 'num_spaces' that takes a sentence as one string
 #    and returns the number of spaces.
+def num_spaces(string):
+    spaces=0
+    for char in string:
+        if char == " ":
+            spaces += 1
+    return spaces
+num_spaces("Balloonicorn is       awesome!")
 
 
 # 8. Write a function called 'total_meal_price' that can be passed a meal price
 #    and a tip percentage. It should return the total amount paid
 #    (price + price * tip). **However:** passing in the tip percentage should
 #    be optional; if not given, it should default to 15%.
+def total_meal_price(meal_price,tip =0.15):
+    return meal_price+(meal_price * tip)
+print(total_meal_price(4))
+
 
 
 # 9. Write a function called 'sign_and_parity' that takes an integer as an
@@ -113,7 +147,21 @@ PART TWO:
 #    unpack what is returned into two variables --- sign and parity (whether
 #    it's even or odd). Print sign and parity.
 
+def sign_and_parity(num):
+    if num >=0:
+        sign = "Positive"
+    else:
+        sign = "Negative"
 
+    if num % 2 == 0:
+        parity = "Even"
+    else:
+        parity = "Odd"
+    return [sign,parity]
+
+my_sign, my_parity = sign_and_parity(7)
+print(my_sign)
+print(my_parity)   
 ###############################################################################
 
 # PART TWO
@@ -121,6 +169,9 @@ PART TWO:
 # 1. Write a function called full_title that takes a name and a job title as
 #    parameters, making it so the job title defaults to "Engineer" if a job
 #    title is not passed in. Return the person's title and name in one string.
+
+def full_title(name,title="Engineer"):
+    return f"{title} {name}"
 
 # 2. Write a function called write_letter that, given a recipient name & job
 #    title and a sender name, prints the following letter:
@@ -130,6 +181,10 @@ PART TWO:
 #
 #    Use the function from #1 to construct the full title for the letter's
 #    greeting.
+def write_letter(recipient_name, job_title, sender_name):
+    receiver = full_title(recipient_name,job_title)
+    print(f"Dear {job_title} {recipient_name}, I think you are amazing! Sincerely, {sender_name}")
+write_letter("Lynne", "graphic designer", "Hackbright")
 
 
 ###############################################################################
